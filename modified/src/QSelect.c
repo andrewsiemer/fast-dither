92580230faa0b2052b41015392daa47355ac6feb
Andrew Spaulding <andyespaulding@gmail.com>
2022-10-13 16:28:17 -0400

    Improved partition implementation to be less mean to the branch predictor

--- modified/src/QSelect.c
+++ modified/src/QSelect.c
@@ -33,47 +33,46 @@ Partition(
     // always be some value <= size/2, due to our start selection.
     size_t lo = 0, hi = size - 1;
     size_t mid_lo = hi >> 1, mid_hi = (hi >> 1) - 1;
-    while (lo <= hi) {
-        if ((mid_lo - lo) > 0) {
-            if (buf[lo] < pivot) {
-                lo++;
-            } else if (buf[lo] > pivot) {
-                SWAP(buf[lo], buf[hi]);
-                if (hi-- == mid_hi) {
-                    // If the mid-point region is empty, we need to move both.
-                    if (((ptrdiff_t)((mid_hi + 1) - mid_lo)) == 0) { mid_lo--; }
-                    mid_hi--;
-                }
-            } else {
-                // Exact match, move middle.
-                mid_lo--;
-                SWAP(buf[lo], buf[mid_lo]);
-            }
-        } else if ((hi - mid_hi) > 0) {
-            if (buf[hi] > pivot) {
-                hi--;
-            } else if (buf[hi] < pivot) {
-                SWAP(buf[lo], buf[hi]);
 
-                // If we're here, lo == mid_lo, so we need to move mid_lo up.
-                // This means the middle loses an element, but we'll fix
-                // it on the next iteration.
-                lo++;
-                if (((ptrdiff_t)((mid_hi + 1) - mid_lo)) == 0) { mid_hi++; }
-                mid_lo++;
-            } else {
-                mid_hi++;
-                SWAP(buf[hi], buf[mid_hi]);
+    // Partition lower half.
+    while ((mid_lo - lo) > 0) {
+        if (buf[lo] < pivot) {
+            lo++;
+        } else if (buf[lo] > pivot) {
+            SWAP(buf[lo], buf[hi]);
+            if (hi-- == mid_hi) {
+                // If the mid-point region is empty, we need to move both.
+                if (((ptrdiff_t)((mid_hi + 1) - mid_lo)) == 0) { mid_lo--; }
+                mid_hi--;
             }
         } else {
-            // Center has been filled with pivot.
-            break;
+            // Exact match, move middle.
+            mid_lo--;
+            SWAP(buf[lo], buf[mid_lo]);
+        }
+    }
+
+    // Partition whatever remains of the top half.
+    while ((hi - mid_hi) > 0) {
+        if (buf[hi] > pivot) {
+            hi--;
+        } else if (buf[hi] < pivot) {
+            SWAP(buf[lo], buf[hi]);
+
+            // If we're here, lo == mid_lo, so we need to move mid_lo up.
+            // This means the middle loses an element, but we'll fix
+            // it on the next iteration.
+            lo++;
+            if (((ptrdiff_t)((mid_hi + 1) - mid_lo)) == 0) { mid_hi++; }
+            mid_lo++;
+        } else {
+            mid_hi++;
+            SWAP(buf[hi], buf[mid_hi]);
         }
     }
 
     // If we didn't find the pivot, yell.
     assert(mid_hi >= mid_lo);
-
     *plo = mid_lo;
     *phi = mid_hi;
 }
