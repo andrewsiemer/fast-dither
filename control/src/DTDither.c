8516045006c3725bdfb93eb4486ea10aafdb3fa3
Blaine Huey <bhthenexttesla@gmail.com>
2022-12-14 12:30:53 -0500

    Untested Dither Control Timing Code

--- control/src/DTDither.c
+++ control/src/DTDither.c
@@ -7,6 +7,9 @@
  */
 
 #include <DTDither.h>
+#include <UtilMacro.h>
+
+#include <assert.h>
 
 typedef struct {
     short int r, g, b;
@@ -16,13 +19,37 @@ DTDiff CalculateDifference(DTPixel original, DTPixel new);
 void ApplyDifference(DTPixel *pixel, DTDiff diff, int factor);
 byte ByteCap(int n);
 
+void DTTimeInit(dt_time_t *time)
+{
+    assert(time);
+    *time = (dt_time_t) {0};
+}
+
+void DTTimeReport(dt_time_t *time)
+{
+    const double dither_theoretical = (1/1.3125);
+
+    double dither_time = TIME_NORM(0, time->dither_time);
+    double dither_pix = ((double)time->dither_units) / dither_time;
+    double dither_peak = (dither_pix / dither_theoretical) * 100;
+
+    printf("Dither%19s%-20.6lf%-20.6lf%.2lf%%\n", "", dither_time, dither_pix, dither_peak);
+}
+
 void
 ApplyFloydSteinbergDither(DTImage *image, DTPalette *palette)
 {
+    dt_time_t t;
+    DTTimeInit(&t);
+
+    unsigned long long ts1, ts2;
+
     for (size_t i = 0; i < image->height; i++) {
         for (size_t j = 0; j < image->width; j++) {
             DTPixel original = image->pixels[i*image->width + j];
             DTPixel new = FindClosestColorFromPalette(original, palette);
+
+            TIMESTAMP(ts1);
 
             DTDiff diff = CalculateDifference(original, new);
 
@@ -44,8 +71,14 @@ ApplyFloydSteinbergDither(DTImage *image, DTPalette *palette)
             }
 
             image->pixels[i*image->width + j] = new;
+
+            TIMESTAMP(ts2);
+            t.dither_time += (ts2 - ts1);
+            t.dither_units += 1;
         }
     }
+
+    DTTimeReport(&t);
 }
 
 DTDiff
